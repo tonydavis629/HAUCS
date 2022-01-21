42628d5ad229373ea937477d8eae4578f291f466
tonydavis629 <acdavis629@gmail.com>
2022-01-21 17:35:59 -0500

    create mission_planner

---
+++ haucs/mission_planner/planner.py
@@ -0,0 +1,9 @@
+#points in a field south of engineering west
+strt=[26.369769461284108, -80.1043028838524]
+point1= [26.369813757198585, -80.1044878865724]
+point2=[26.369738025463516, -80.10448948142343]
+
+#give points
+#points are processed to sample waypoints and transition waypoints
+#then load those into mission_planner
+#then load into sitl
