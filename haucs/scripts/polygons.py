34585803b704a5d193e2d373df0b1f5ce43dc8b6
tonydavis629 <acdavis629@gmail.com>
2022-01-28 14:57:56 -0500

    added matlab script

---
+++ haucs/scripts/polygons.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append("..")
+from data.dataset import PondsDataset, polygon, ponds, plot_poly, plot_pts
+import matplotlib.pyplot as plt
+
+polygons = polygon(num_vrtx=4, xlims=[0, 1], ylims=[0, 1])
+multipoly, vertices= polygons.create_polygons(3)
+plot_poly(multipoly)
+print(vertices)
+plt.show()
---
+++ haucs/scripts/shamos.m
@@ -0,0 +1,4 @@
+%poly
+env_path = '/home/tony/miniconda3/envs/haucs/bin/python'; %change to your python env exe. Make sure to install depencies
+pyenv('Version', env_path)
+pyrunfile('polygons.py')
