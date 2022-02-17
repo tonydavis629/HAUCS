ef4cd5366d6303a77a3e0781aec5ed80d8f1c5e2
tonydavis629 <acdavis629@gmail.com>
2022-02-17 13:43:30 -0500

    shamos.py to load data into matlab

--- scripts/shamos.py
+++ scripts/shamos.py
@@ -1,6 +1,9 @@
-from haucs.data.dataset import PondsDataset, arr2cord, polygon, ponds, plot_poly, plot_pts
+from haucs.data.dataset import polygon, ponds
 
-polygons = polygon(num_vrtx=4, xlims=[0, 1], ylims=[0, 1])
-multipoly, vertices= polygons.create_polygons(num_polygons=3)
-pondset = ponds(density=5,polygon=multipoly, depot_loc=[.5,.5])
-print(pondset.loc)
+def shamos():
+    polygons = polygon(num_vrtx=4, xlims=[0, 1], ylims=[0, 1])
+    multipoly, vertices= polygons.create_polygons(num_polygons=3)
+    pondset = ponds(num_pts=100,polygon=multipoly) 
+    return vertices
+
+
