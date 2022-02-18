b26ef195275230ddfdf301ddf2fafe37631e8ab2
tonydavis629 <acdavis629@gmail.com>
2022-02-18 17:21:55 -0500

    python talking to matlab

--- scripts/shamos.py
+++ scripts/shamos.py
@@ -1,12 +1,9 @@
 from haucs.data.dataset import polygon, ponds
+import scipy.io as sio 
 
 def shamos():
     polygons = polygon(num_vrtx=4, xlims=[0, 1], ylims=[0, 1])
     multipoly, vertices= polygons.create_polygons(num_polygons=3)
     pondset = ponds(num_pts=100,polygon=multipoly) 
-    return vertices
-
-
-def test():
-    print('testing')
-    return 'test'
+    vertponddic = {'vertices':vertices, 'ponds':pondset.loc}
+    sio.savemat('ponds.mat',vertponddic)
--- scripts/srijita.m
+++ scripts/srijita.m
@@ -1,15 +1,2 @@
-shamos = py.shamos.test();
-% py.testmatlab.test()
-% test = pyrunfile('testmatlab.py');
-% vertices = shamos(1);
-% ponds = shamos(2);
-% 
-% vert_mat = cell(vertices);
-% % 
-% vrts = cell2struct(vert_mat, 'vertices', 2);
-% % 
-% list = vrts.vertices;
+py.shamos.shamos();
 
-% for i=1:length(list)
-%     print(list(i))
-% end
