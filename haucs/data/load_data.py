824d4b27d6b4021b3dd712cb6167a5c8f943fdde
Tony Davis <acdavis629@gmail.com>
2022-08-30 16:45:43 -0700

    update for valid ponds

--- haucs/data/load_data.py
+++ haucs/data/load_data.py
@@ -7,19 +7,20 @@ import numpy as np
 
 if __name__ == "__main__":
     
-    coords = np.loadtxt('C:\\Users\\anthonydavis2020\\Documents\github\\HAUCS\\haucs\\pondsmall.txt')
+    coords = np.loadtxt('C:\\Users\\coral-computer\\Documents\\github\\HAUCS\\haucs\\ponds.txt')
     ILponds, lat_range, long_range = coord2arr(coords)
+    np.savetxt('C:\\Users\\coral-computer\\Documents\\github\\HAUCS\\haucs\\ILnormcoordsmall.txt',ILponds,delimiter=',',fmt='%f')
     # ILponds = np.loadtxt('C:\\Users\\anthonydavis2020\\Documents\\github\\HAUCS\\haucs\\ILnormcoordsmall.txt', delimiter=',')
 
     data = PondsDataset(farms=1, num_pts=5, xlims=[0, 1], ylims=[0, 1])
     data.data = ILponds
     
-    # sized_ATSP_ds = data.load_ATSP_dataset()
-    # with open('C:\\Users\\anthonydavis2020\\Documents\\github\\HAUCS\\haucs\\ATSP_IL.pkl', 'wb') as f:
-    #     pickle.dump(sized_ATSP_ds, f, pickle.HIGHEST_PROTOCOL)
+    sized_ATSP_ds = data.load_ATSP_dataset()
+    with open('C:\\Users\\coral-computer\\Documents\\github\\HAUCS\\haucs\\ATSP_IL.pkl', 'wb') as f:
+        pickle.dump(sized_ATSP_ds, f, pickle.HIGHEST_PROTOCOL)
     
     GLOP = data.load_GLOP_dataset()
-    with open('C:\\Users\\anthonydavis2020\\Documents\\github\\HAUCS\\haucs\\GLOP_dataset_ILsmall.pkl', 'wb') as f:
+    with open('C:\\Users\\coral-computer\\Documents\\github\\HAUCS\\haucs\\GLOP_dataset_IL.pkl', 'wb') as f:
         pickle.dump(GLOP, f, pickle.HIGHEST_PROTOCOL)
 
     # HPP data written in matlab in solvers/HPP
--- haucs/save_routes.py
+++ haucs/save_routes.py
@@ -2,14 +2,14 @@ from haucs.utils.utils import coord2arr
 import numpy as np
 import pickle
 
-coords = np.loadtxt('./ponds.txt') #first point is the depot
+coords = np.loadtxt('C:\\Users\\coral-computer\\Documents\\github\\HAUCS\\haucs\\ponds.txt') #first point is the depot
 
 #run data/load_data.py to generate the data for each model
 
 #run solvers/HPP/solve.m first
 # # # # # # HPP # # # # # # # 
 # depot is the first point of each route
-with open('HPProutes.pkl','rb') as routes:
+with open('C:\\Users\\coral-computer\\Documents\\github\\HAUCS\\haucs\\HPProutes.pkl','rb') as routes:
     all_routes = pickle.load(routes)
     all_routes = all_routes.squeeze()
 
@@ -20,37 +20,37 @@ tour = [] #used for plotting in atsp
 for i, route in enumerate(solved_routes):
     tour.extend(list(route[1:]) + [0])
     final_route = coords[route,:] 
-    np.savetxt('./HPProutes'+str(i)+'.txt',final_route,delimiter=',',fmt='%f')
+    np.savetxt('C:\\Users\\coral-computer\\Documents\\github\\HAUCS\\haucs\\HPProutes'+str(i)+'.txt',final_route,delimiter=',',fmt='%f')
     
 tour = tour[:-1] # remove last zero
-with open('HPPtour.pkl','wb') as tourfile:
+with open('C:\\Users\\coral-computer\\Documents\\github\\HAUCS\\haucs\\HPPtour.pkl','wb') as tourfile:
     pickle.dump(tour,tourfile)
 print(tour)
 
 # run atsp/plot_vrp.ipynb
 # # # # # # # GM # # # # # # # # 
 # no depot in routes
-# with open('GM_routes.pkl','rb') as routes:
-#     all_routes = pickle.load(routes)
-#     all_routes = all_routes[0]
+with open('GM_routes.pkl','rb') as routes:
+    all_routes = pickle.load(routes)
+    all_routes = all_routes[0]
     
-# for i, route in enumerate(all_routes):
-#     final_route = coords[route,:]
-#     final_route = np.insert(final_route,0,coords[0],axis=0) #insert depot
-#     np.savetxt('./GMroutes'+str(i)+'.txt',final_route,delimiter=',',fmt='%f')
+for i, route in enumerate(all_routes):
+    final_route = coords[route,:]
+    final_route = np.insert(final_route,0,coords[0],axis=0) #insert depot
+    np.savetxt('C:\\Users\\coral-computer\\Documents\\github\\HAUCS\\haucs\\GMroutes'+str(i)+'.txt',final_route,delimiter=',',fmt='%f')
     
     
 # # # # # # # # GLOP # # # # # # # # # 
 # depot is the first point of each route
-# with open('GLOP_routes_IL.pkl','rb') as routes:
-#     all_routes = pickle.load(routes)
-#     all_routes = all_routes[0]
+with open('GLOP_routes_IL.pkl','rb') as routes:
+    all_routes = pickle.load(routes)
+    all_routes = all_routes[0]
     
-# tour = []
-# for i, route in enumerate(all_routes):
-#     tour.extend(list(route[1:]) + [0])
-#     final_route = coords[route,:]
-#     np.savetxt('./GLOProutes'+str(i)+'.txt',final_route,delimiter=',',fmt='%f')
+tour = []
+for i, route in enumerate(all_routes):
+    tour.extend(list(route[1:]) + [0])
+    final_route = coords[route,:]
+    np.savetxt('C:\\Users\\coral-computer\\Documents\\github\\HAUCS\\haucs\\GLOProutes'+str(i)+'.txt',final_route,delimiter=',',fmt='%f')
 
-# tour = tour[:-1] # remove last zero
-# print(tour)
+tour = tour[:-1] # remove last zero
+print(tour)
