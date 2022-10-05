5a3b3a71ad3c715acfe282eb00990e824d1ea9ee
Tony Davis <acdavis629@gmail.com>
2022-10-05 13:52:51 -0400

    cleaning up HPP

--- haucs/solvers/HPP/shamos.py
+++
@@ -1,9 +0,0 @@
-from haucs.data.dataset import polygon, ponds
-import scipy.io as sio 
-
-def shamos():
-    polygons = polygon(num_vrtx=4, xlims=[0, 1], ylims=[0, 1])
-    multipoly, vertices= polygons.create_polygons(num_polygons=3)
-    pondset = ponds(num_pts=100,polygon=multipoly) 
-    vertponddic = {'vertices':vertices, 'ponds':pondset.loc}
-    sio.savemat('ponds.mat',vertponddic)
